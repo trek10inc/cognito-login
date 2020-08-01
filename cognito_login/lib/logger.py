60247b0172f7806be662a956df794c17704ed909
Michael Barney, Jr <mbarneyme@gmail.com>
2020-08-01 10:34:59 -0400

    fixed logger name

--- cognito_login/lib/logger.py
+++ cognito_login/lib/logger.py
@@ -1,7 +1,7 @@
 """Logging utilities"""
 import logging
 
-logger = logging.getLogger('awsume') # type: logging.Logger
+logger = logging.getLogger('cognito-login') # type: logging.Logger
 LOG_HANDLER = logging.StreamHandler()
 LOG_HANDLER.setFormatter(logging.Formatter('[%(asctime)s] %(filename)s:%(funcName)s : [%(levelname)s] %(message)s'))
 logger.addHandler(LOG_HANDLER)
